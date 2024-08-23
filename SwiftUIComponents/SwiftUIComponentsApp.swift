7b8c3a818e1306bacf83b02d29c6218582b6e8e4
Laith Abdo <labdo@access2arabia.com>
2024-08-23 20:45:50 +0300

    First Commit

---
+++ SwiftUIComponents/SwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsApp.swift
+//  SwiftUIComponents
+//
+//  Created by Laith Abdo on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
